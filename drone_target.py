0dc364c8ca5db0f649f0af998cd335a4a34aaa1d
flurbudurbur <sebastianhoogendijk@gmail.com>
2023-09-21 13:31:47 +0200

    push

--- drone_target.py
+++ drone_target.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 
 class DroneTracker(FaceDetect):
+
+
     def follow(self):
         # get coordinates of the face
         try:
@@ -38,15 +40,55 @@ class DroneTracker(FaceDetect):
             print('stop up/down')
 
         # get the face 
-
-        bgr_boundaries = [([51, 205, 50], [63, 255, 62])]
-        for lower, upper in bgr_boundaries:
-            lower = np.array(lower, dtype = "uint8")
-            upper = np.array(upper, dtype = "uint8")
+        
+        # bgr_boundaries = [([51, 205, 50], [63, 255, 62])]
+        # for lower, upper in bgr_boundaries:
+        #     lower = np.array(lower, dtype = "uint8")
+        #     upper = np.array(upper, dtype = "uint8")
 
             # mask = cv2.inRange(, lower, upper)
 
+        while True:
+            faceframe = coords[0]
+            self.stream = self.canvas.cvtColor(self.frame, self.canvas.COLOR_BGR2HSV)
+            height, width, _ = frame.shape
+            cx = int(coords[2] / 2)
+            cy = int(coords[3] / 2)
+
+            # Pick pixel value
+            pixel_center = hsv_frame[cy, cx]
+            hue_value = pixel_center[0]
+
+            color = "Undefined"
+            if hue_value < 5:
+                color = "RED"
+            elif hue_value < 22:
+                color = "ORANGE"
+            elif hue_value < 33:
+                color = "YELLOW"
+            elif hue_value < 78:
+                color = "GREEN"
+            elif hue_value < 131:
+                color = "BLUE"
+            elif hue_value < 170:
+                color = "VIOLET"
+            else:
+                color = "RED"
+
+            pixel_center_bgr = frame[cy, cx]
+            b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+
+            self.canvas.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
+            self.canvas.putText(frame, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
+            self.canvas.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
+
+            self.canvas.imshow("Frame", frame)
+            key = self.canvas.waitKey(1)
+            if key == 27:
+                break
+
 facedetector = DroneTracker({'custom': 'follow'})
+# facedetector = DroneTracker({'custom': 'colorfind'})
 
 try:
     facedetector.start()
